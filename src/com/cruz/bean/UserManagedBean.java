529087779eadd0d209da8ec0b2a729b0ca22f544
Eren Çetinkaya <erencetinkaya.ceng@gmail.com>
2019-03-08 21:49:56 +0300

    Kullanıcı ekleme ve düzenleme işlemleri yapıldı. Bootstrap eklendi.

---
+++ src/com/cruz/bean/UserManagedBean.java
@@ -0,0 +1,45 @@
+package com.cruz.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import com.cruz.dao.IUserRepository;
+import com.cruz.dao.impl.UserRepository;
+import com.cruz.model.User;
+
+@ManagedBean
+@SessionScoped
+public class UserManagedBean {
+
+	private User user;
+	private IUserRepository userRepository;
+
+	public UserManagedBean() {
+		user = new User();
+		userRepository = new UserRepository();
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String save(UserManagedBean umb) {
+		userRepository.saveUser(umb.getUser());
+		return "index?faces-redirect=true";
+	}
+
+	public String update(int id) {
+		user = userRepository.findUserById(id);
+		return "editUser?faces-redirect=true";
+	}
+
+	public String update(UserManagedBean umb) {
+		userRepository.updateUser(umb.getUser());
+		return "index?faces-redirect=true";
+	}
+
+}
